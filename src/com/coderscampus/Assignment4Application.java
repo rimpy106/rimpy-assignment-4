38eec88f3e519b9386ce21fc90a187552ce7cdba
rimpy106 <rimpy106@gmail.com>
2022-02-01 16:51:43 -0600

    UpdateSortandFilewriterCode

--- src/com/coderscampus/Assignment4Application.java
+++ src/com/coderscampus/Assignment4Application.java
@@ -1,6 +1,8 @@
 package com.coderscampus;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Assignment4Application {
 
@@ -8,8 +10,19 @@ public class Assignment4Application {
 		Student[] students = new Student[101];
 		FileService fileservice = new FileService();
 		students=fileservice.readStudentsFromFile("student-master-list.csv");
+		Arrays.sort(students, new Comparator<Student>() {
+			@Override
+			public int compare(Student o1, Student o2) {
+				/*
+				 * Integer stud1_grade=Integer.parseInt(o1.getGrade()); Integer
+				 * stud2_grade=Integer.parseInt(o2.getGrade());
+				 */
+				return o2.getGrade().compareTo(o1.getGrade());
+			}
 		
-		
+	});
+		 
+		fileservice.writeStudentstoFile(students);
 		/*
 		 * for(int i=1;i<students.length;i++) {
 		 * System.out.print(students[i].getStudent_id());
@@ -17,8 +30,5 @@ public class Assignment4Application {
 		 * System.out.print(students[i].getCourse());
 		 * System.out.print(students[i].getGrade()); System.out.println(); }
 		 */
-		 
-		fileservice.writeStudentstoFile(students);
-	}
-
+}
 }
--- src/com/coderscampus/FileService.java
+++ src/com/coderscampus/FileService.java
@@ -9,11 +9,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.Arrays;
 
 public class FileService {
 	private static final String COMMA_DELIMITER = ",";
+	private static final String fileHeader="Student ID,Student Name, Course,Grade \n";
 
-	public Student[] readStudentsFromFile(String file) {
+	public Student[] readStudentsFromFile(String file) throws IOException {
 		BufferedReader filereader = null;
 		Student[] students = new Student[101];
 		try {
@@ -25,54 +27,67 @@ public class FileService {
 				students[i] = new Student(lineData[0], lineData[1], lineData[2], lineData[3]);
 				i++;
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} finally {
+			if (filereader != null)
+				filereader.close();
 		}
 		return students;
-
 	}
 
 	public void writeStudentstoFile(Student[] students) throws IOException {
-		BufferedWriter filewriter = null;
-		for (int i = 1; i < students.length; i++) {
-			if (students[i] != null) {
-				if (students[i].getCourse().contains("COMPSCI")) {
-					// System.out.println(students[i].toString());
-					filewriter = new BufferedWriter(new FileWriter("course1.csv"));
-					filewriter.write(students[i].getStudent_id());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getStudent_name());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getCourse());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getGrade());
-					filewriter.write("\n");
-				} else if (students[i].getCourse().contains("APMTH")) {
-					filewriter = new BufferedWriter(new FileWriter("course2.csv"));
-					filewriter.write(students[i].getStudent_id());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getStudent_name());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getCourse());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getGrade());
-					filewriter.write("\n");
-				} else if (students[i].getCourse().contains("STAT")) {
-					filewriter = new BufferedWriter(new FileWriter("course3.csv"));
-					filewriter.write(students[i].getStudent_id());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getStudent_name());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getCourse());
-					filewriter.write(COMMA_DELIMITER);
-					filewriter.write(students[i].getGrade());
-					filewriter.write("\n");
-				}
+		BufferedWriter filewriter1 = new BufferedWriter(new FileWriter("course1.csv"));
+		filewriter1.write(fileHeader);
+		BufferedWriter filewriter2 = new BufferedWriter(new FileWriter("course2.csv"));
+		filewriter2.write(fileHeader);
+		BufferedWriter filewriter3 = new BufferedWriter(new FileWriter("course3.csv"));
+		filewriter3.write(fileHeader);
+		try {
+			for (int i = 1; i < students.length; i++) {
+				if (students[i] != null) {
+					//int grade=Integer.parseInt(students[i].getGrade());
+					if (students[i].getCourse().contains("COMPSCI")) {
+						// System.out.println(students[i].toString());
+						filewriter1.write(students[i].getStudent_id());
+						filewriter1.write(COMMA_DELIMITER);
+						filewriter1.write(students[i].getStudent_name());
+						filewriter1.write(COMMA_DELIMITER);
+						filewriter1.write(students[i].getCourse());
+						filewriter1.write(COMMA_DELIMITER);
+						filewriter1.write(students[i].getGrade());
+						filewriter1.write("\n");
+						//Arrays.sort(grade);
+					} else if (students[i].getCourse().contains("APMTH")) {
+						filewriter2.write(students[i].getStudent_id());
+						filewriter2.write(COMMA_DELIMITER);
+						filewriter2.write(students[i].getStudent_name());
+						filewriter2.write(COMMA_DELIMITER);
+						filewriter2.write(students[i].getCourse());
+						filewriter2.write(COMMA_DELIMITER);
+						filewriter2.write(students[i].getGrade());
+						filewriter2.write("\n");
+					} else if (students[i].getCourse().contains("STAT")) {
+						filewriter3.write(students[i].getStudent_id());
+						filewriter3.write(COMMA_DELIMITER);
+						filewriter3.write(students[i].getStudent_name());
+						filewriter3.write(COMMA_DELIMITER);
+						filewriter3.write(students[i].getCourse());
+						filewriter3.write(COMMA_DELIMITER);
+						filewriter3.write(students[i].getGrade());
+						filewriter3.write("\n");
+					}
 
+				}
 			}
+		} finally {
+			if (filewriter1 != null)
+				filewriter1.close();
+			if (filewriter2 != null)
+				filewriter2.close();
+			if (filewriter3 != null)
+				filewriter3.close();
 		}
 	}
+	
+	
 
 }
